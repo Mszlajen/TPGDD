770dd3c81ead548dd58830b7e950a87d1949fe95
mszlajen <mszlajen@est.frba.utn.edu.ar>
2018-10-02 18:49:40 -0300

    Forms

---
+++ src/Abm Empresa Espectaculo/Selection.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PalcoNet.Abm_Cliente
+{
+    partial class Selection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.ModifyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¿Qué desea hacer?";
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(16, 30);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(98, 52);
+            this.CreateButton.TabIndex = 1;
+            this.CreateButton.Text = "Crear compañia";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            // 
+            // ModifyButton
+            // 
+            this.ModifyButton.Location = new System.Drawing.Point(128, 30);
+            this.ModifyButton.Name = "ModifyButton";
+            this.ModifyButton.Size = new System.Drawing.Size(98, 52);
+            this.ModifyButton.TabIndex = 2;
+            this.ModifyButton.Text = "Modificar compañia";
+            this.ModifyButton.UseVisualStyleBackColor = true;
+            // 
+            // Selection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 94);
+            this.Controls.Add(this.ModifyButton);
+            this.Controls.Add(this.CreateButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Selection";
+            this.Text = "Selección";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button ModifyButton;
+    }
+}
--- src/RolSelection.Designer.cs
+++ src/RolSelection.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.RolesBox = new System.Windows.Forms.ComboBox();
             this.AcceptButton = new System.Windows.Forms.Button();
+            this.LogoutButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -52,18 +53,28 @@
             // 
             // AcceptButton
             // 
-            this.AcceptButton.Location = new System.Drawing.Point(99, 58);
+            this.AcceptButton.Location = new System.Drawing.Point(53, 56);
             this.AcceptButton.Name = "AcceptButton";
             this.AcceptButton.Size = new System.Drawing.Size(81, 34);
             this.AcceptButton.TabIndex = 2;
             this.AcceptButton.Text = "Ingresar";
             this.AcceptButton.UseVisualStyleBackColor = true;
             // 
+            // LogoutButton
+            // 
+            this.LogoutButton.Location = new System.Drawing.Point(151, 56);
+            this.LogoutButton.Name = "LogoutButton";
+            this.LogoutButton.Size = new System.Drawing.Size(75, 34);
+            this.LogoutButton.TabIndex = 4;
+            this.LogoutButton.Text = "Cerrar Sesion";
+            this.LogoutButton.UseVisualStyleBackColor = true;
+            // 
             // RolSelection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 104);
+            this.Controls.Add(this.LogoutButton);
             this.Controls.Add(this.AcceptButton);
             this.Controls.Add(this.RolesBox);
             this.Controls.Add(this.label1);
@@ -79,5 +90,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox RolesBox;
         private System.Windows.Forms.Button AcceptButton;
+        private System.Windows.Forms.Button LogoutButton;
     }
 }
